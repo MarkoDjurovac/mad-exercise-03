6ba25d798bb23336d0c21ca56eda498afac0207a
MarkoDjurovac <marko.djurovac@stud.fh-campuswien.ac.at>
2024-03-15 13:50:05 +0100

    Simplified DetailScreen.

--- app/src/main/java/com/example/movieappmad24/ui/screens/DetailScreen.kt
+++ app/src/main/java/com/example/movieappmad24/ui/screens/DetailScreen.kt
@@ -6,7 +6,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
@@ -14,33 +13,29 @@ import com.example.movieappmad24.ui.widgets.ImageCarousel
 import com.example.movieappmad24.ui.movie.MovieRow
 import com.example.movieappmad24.ui.navigation.SimpleBottomAppBar
 import com.example.movieappmad24.ui.navigation.SimpleTopAppBar
-import com.example.movieappmad24.models.Movie
 import com.example.movieappmad24.models.getMovieById
 
 @Composable
 fun DetailScreen(movieId: String?, navController: NavController) {
-    var movie: Movie? = null
-
     if( movieId != null) {
-        movie = getMovieById(movieId);
-    }
-
-    Scaffold(
-        topBar = { SimpleTopAppBar(
-            title = movie?.title ?: "Movie detail",
-            navController
-        ) },
-        bottomBar = { SimpleBottomAppBar(navController) }
-    ) { paddingValues ->
-        Surface(
-            modifier = Modifier.fillMaxSize().padding(paddingValues),
-            color = MaterialTheme.colorScheme.background
-        ) {
-            Column{
-                if(movie != null) {
-                    MovieRow(movie)
-                    ImageCarousel(movie)
-                } else Text("Movie not found!")
+        val movie = getMovieById(movieId);
+        if(movie != null) {
+            Scaffold(
+                topBar = { SimpleTopAppBar(
+                    title = movie.title,
+                    navController
+                ) },
+                bottomBar = { SimpleBottomAppBar(navController) }
+            ) { paddingValues ->
+                Surface(
+                    modifier = Modifier.fillMaxSize().padding(paddingValues),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Column {
+                        MovieRow(movie)
+                        ImageCarousel(movie)
+                    }
+                }
             }
         }
     }
