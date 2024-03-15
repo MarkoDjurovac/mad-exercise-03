a7abd085789df5b20c8474f418123a776452c01d
MarkoDjurovac <marko.djurovac@stud.fh-campuswien.ac.at>
2024-03-15 13:45:10 +0100

    Remove unused imports.

--- app/src/main/java/com/example/movieappmad24/ui/widgets/ImageCarousel.kt
+++ app/src/main/java/com/example/movieappmad24/ui/widgets/ImageCarousel.kt
@@ -1,26 +1,17 @@
 package com.example.movieappmad24.ui.widgets
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import com.example.movieappmad24.models.Movie
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ImageCarousel(movie: Movie) {
     LazyRow {
